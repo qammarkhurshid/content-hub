f9578a8b9f8d83b402f2437e1f2cd18d3206f802
qammarkhurshid <qamar.khurshid@gmail.com>
2022-07-01 08:37:10 +0500

    Setup basic project skeleton

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+module.exports = {
+  app
+};
+// app.listen(3000, ()=>{
+//   console.log('=================================');
+//   console.log(`***SERVER STARTED ON PORT ${3000}***`);
+//   console.log('=================================');
+// });
